6ba5af982ce82c7b1e1fdb03c16fa34720e74bfd
jokinson <joaquin.martin@opendeusto.es>
2022-11-19 19:35:08 +0100

    First Commit

---
+++ SeankersIWApp/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Sneaker
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'PaginaPrincipal.html')
+
