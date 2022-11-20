5b2c945165e51868dc0ec5fcea33f4899ccf170d
jokinson <joaquin.martin@opendeusto.es>
2022-11-20 18:22:34 +0100

    commit

--- SeankersIWApp/views.py
+++ SeankersIWApp/views.py
@@ -5,7 +5,7 @@ from .models import Sneaker
 # Create your views here.
 
 def index(request):
-    return render(request, 'PaginaPrincipal.html')
+    return render(request, 'index.html')
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, '.html')
