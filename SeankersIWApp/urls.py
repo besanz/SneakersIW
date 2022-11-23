022ee0cd9df91d98ee6961222eaf91511511dc81
besanz <benat.sanz@opendeusto.es>
2022-11-23 23:30:28 +0100

    el jo king

--- SeankersIWApp/urls.py
+++ SeankersIWApp/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-   # path('', views.index, name='index'),
-    path('', views.index, name='prueba'),
-    path('Home', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('Products', views.products, name='products'),
+    path('About', views.about, name='about'),
+    path('Faqs', views.faqs, name='faqs'),
+    path('Carrito', views.carrit, name='carrit'),
+    path('Detalles_de_producto', views.pDetail, name='productDetail')
     
 
 ]
--- SeankersIWApp/views.py
+++ SeankersIWApp/views.py
@@ -7,5 +7,17 @@ from .models import Sneaker
 def index(request):
     return render(request, 'index.html')
 
-def home(request):
-    return render(request, '.html')
+def products(request):
+    return render(request, 'products.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def faqs(request):
+    return render(request, 'faqs.html')
+
+def carrit(request):
+    return render(request, 'shoppingcart.html')
+
+def pDetail(request):
+    return render(request, 'productdetail.html')
