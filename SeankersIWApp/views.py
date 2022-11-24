333f8f70ced8ad3491c2812e77da6d620625e8a5
jokinson <joaquin.martin@opendeusto.es>
2022-11-24 13:42:03 +0100

    commit

--- SeankersIWApp/views.py
+++ SeankersIWApp/views.py
@@ -1,11 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Sneaker
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
-def home(request):
-    return render(request, '.html')
+def products(request):
+    return render(request, 'products.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def faqs(request):
+    return render(request, 'faqs.html')
+
+def carrit(request):
+    return render(request, 'shoppingcart.html')
+
+def pDetail(request):
+    return render(request, 'productdetail.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
--- SneakersIW/urls.py
+++ SneakersIW/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('SeankersIWApp/', include('SeankersIWApp.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
